5a294c0fbb491c85cd07d67a3ce99a4f66d93586
DenisCuenca <dcbuele@gmail.com>
2023-07-17 18:44:45 -0500

    cambios

--- taller/taller/edificios/serializer.py
+++ taller/taller/edificios/serializer.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from .models import Edificio, Departamento
+from .models import Edificio, Departamento, Propietario
 
 from rest_framework import serializers
 
@@ -14,3 +14,18 @@ class DepartamentoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Departamento
         fields = "__all__"
+
+
+class PropietarioSerializer(serializers.HyperlinkedModelSerializer):
+    departamentos = serializers.SerializerMethodField()
+    edificios_nombres = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Propietario
+        fields = "__all__"
+
+    def get_departamentos(self, obj):
+        return obj.obtener_numDeps()
+
+    def get_edificios_nombres(self, obj):
+        return obj.edifnombres()
